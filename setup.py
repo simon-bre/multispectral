bfa55d8fcf01bcb89bb03fd458fc6591d3361f31
Simon Brenner <sbrenner@caa.tuwien.ac.at>
2019-04-05 21:53:49 +0200

    second!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='multispectral',
       version='0.0',
       description='A convenience package for basic operations on multispectral images.',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/simon-bre/multispectral',
       author='Simon Brenner',
       author_email='sbrenner@cvl.tuwien.ac.at',
       license='MIT',
